0659040ea00d611abb946a5a9b22745e28dd3bce
William Waites <william.waites@lshtm.ac.uk>
2020-12-10 09:10:01 +0000

    refactor household model for speed and clarity

--- stamford/house.py
+++ stamford/house.py
@@ -7,8 +7,13 @@
 # Edit 7 Oct: Try to debug the sub-epidemics problem Lorenzo noticed
 # Remember to atleast_2d the design matrices!
 
-import numba
+## have to seed the random number generator first otherwise we get different
+## results depending on which numba options are turned on or off.
 import numpy as np
+np.random.seed(42)
+
+from numba import jit, njit, prange
+from numba.typed import List
 import networkx as nx
 import scipy.stats as st
 import scipy.optimize as op
@@ -25,7 +30,19 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO,
                     format='%(asctime)s - %(name)s:%(levelname)s - %(message)s')
 log = logging.getLogger(__name__)
 
-def read_graph_data(filename, cutoff=(0, 20)):
+def feature_age_band(g, n, band=(13,30)):
+    """
+    Return a feature vector for the given individual according
+    to whether they are in the provided age band or not. The
+    vector is 1-dimensional.
+    """
+    age = g.nodes[n]['age']
+    vec = np.zeros(1)
+    vec[0] = 1 if age >= band[0] and age <= band[1] else 0
+    return vec
+feature_age_band.size = 1
+
+def read_graph_data(filename, feature, **args):
     """
     Read a GraphML file and produce Y and XX.
 
@@ -34,17 +51,20 @@ def read_graph_data(filename, cutoff=(0, 20)):
       and a 0 otherwise.
 
     XX is a list containing a matrix for each household. The household matrix
-      is of size n x m where n is the size of the household, and m is one
-      less than the number of age categories.
+      is of size n x m where n is the size of the household, and m is the
+      number of features.
 
-    It is unclear why XX should be encoded in this way as this seems very
-    prone to off-by-one errors. Nevertheless, this is what the mynll()
-    function below expects.
+    The argument `feature` is a callable that is called as `feature(g, n, **args)`
+    it takes the graph and a node id that corresponds to an individual, and
+    some optional arguments that are opaquely passed through. It must return
+    a feature vector for that individual.
     """
     g = nx.read_graphml(filename)
 
     ## these are the criteria by which to decide if an individial has been
-    ## infected. any will do.
+    ## infected. any will do. this could usefully be passed in as an argument
+    ## rather than specified here which would allow for different criteria
+    ## or observable data.
     swabs = nx.get_node_attributes(g, "swab.test.result")
     swabscreens = nx.get_node_attributes(g, "swab.test.screening.result")
     seros = nx.get_node_attributes(g, "serology.test.result")
@@ -58,131 +78,173 @@ def read_graph_data(filename, cutoff=(0, 20)):
                 return True
         return False
 
-    Y = numba.typed.List()
-    XX = numba.typed.List()
-
-    lower, upper = cutoff
-    def age_of_interest(a):
-        return a >= lower and a <= upper
+    Y = List()
+    XX = List()
 
     for hh in households(g):
-        ms = list(members(g, hh))
-        n = len(ms)
-        age_groups = [1 if age_of_interest(ages[m]) else 0 for m in ms]
-        myx = np.zeros((n, 1)) ### XXX this 1 should not be hardcoded
-        myy = np.zeros(n)
-        for j, a in enumerate(age_groups):
-            if a > 0:
-                myx[j, a-1] = 1
-            if is_positive(ms[j]):
-                myy[j] = 1
+        myx = np.vstack([feature(g, m, **args) for m in members(g, hh)])
+        myy = np.array([1 if is_positive(m) else 0 for m in members(g, hh)])
+
+        # Sort the household grouping by outcome
+        ii = np.argsort(myy)
+        myx = myx[ii, :]
+        myy = myy[ii]
+
         Y.append(myy)
         XX.append(myx)
+
     return Y, XX
 
 
-@numba.jit(nopython=True, cache=True)
+@njit(cache=True)
 def phi(s, logtheta=0.0):
     theta = np.exp(logtheta)
     return (1.0 + theta * s) ** (-1.0 / theta)
 
 
-@numba.jit(nopython=True, cache=True)
+@njit(cache=True)
 def decimal_to_bit_array(d, n_digits):
     powers_of_two = 2 ** np.arange(32)[::-1]
     return ((d & powers_of_two) / powers_of_two)[-n_digits:]
 
-@numba.jit(nopython=True, parallel=False, fastmath=False, cache=True)
-def mynll(x, Y, XX):
-    na = 1 ### XXX the maximum number identifying subpopulations
-           ### probably better passed in as an argument
-    if True:  # Ideally catch the linear algebra fail directly
-        llaL = x[0]
-        llaG = x[1]
-        logtheta = x[2]
-        eta = (4.0 / np.pi) * np.arctan(x[3])
-        alpha = x[4 : (4 + na)]
-        beta = x[(4 + na) : (4 + 2 * na)]
-        gamma = x[(4 + 2 * na) :]
+@njit(fastmath=True, cache=True)
+def nll(feats, outcomes, llaL, llaG, logtheta, eta, alpha, beta, gamma):
+    """
+    Compute the negative log-likelihood for a specific household.
+    """
+    ## if no infections, we can compute the log-likelihood directly
+    if np.all(outcomes == 0.0):
+        return np.exp(llaG) * np.sum(np.exp(alpha @ (feats.T)))
 
-        num_households = len(Y)
-        nlv = np.zeros(num_households)  # Vector of negative log likelihoods
-        for i in range(0, num_households):
-            y = Y[i]
-            X = XX[i]
-            if np.all(y == 0.0):
-                nlv[i] = np.exp(llaG) * np.sum(np.exp(alpha @ (X.T)))
-            else:
-                # Sort to go zeros then ones WLOG (could do in pre-processing)
-                ii = np.argsort(y)
-                y = y[ii]
-                X = X[ii, :]
-                q = np.sum(y > 0)
-                r = 2 ** q
-                m = len(y)
+    q = np.sum(outcomes > 0)
+    r = 2**q
+    m = outcomes.shape[0]    ## number of household members
 
-                # Quantities that don't vary through the sum
-                Bk = np.exp(-np.exp(llaG) * np.exp(alpha @ (X.T)))
-                laM = np.exp(llaL) * np.outer(
-                    np.exp(beta @ (X.T)), np.exp(gamma @ (X.T))
+    # Quantities that don't vary through the sum
+    Bk = np.exp(-np.exp(llaG) * np.exp(alpha @ (feats.T)))
+    laM = np.exp(llaL) * np.outer(
+        np.exp(beta @ (feats.T)), np.exp(gamma @ (feats.T))
+    )
+    laM *= m ** eta
+
+    BB = np.zeros((r, r))  # To be the Ball matrix
+    for jd in range(0, r):
+        j = decimal_to_bit_array(jd, m)
+        for omd in range(0, jd + 1):
+            om = decimal_to_bit_array(omd, m)
+            if np.all(om <= j):
+                ## divide by zero encountered in double_scalars
+                ## overflow encountered in double_scalars
+                my_phi = phi((1-j) @ laM, logtheta)
+
+                if np.any(
+                    np.floor(np.log10(np.abs(my_phi[my_phi != 0]))) < -100
+                ):
+                    return np.inf
+
+                BB[jd, omd] = 1.0 / np.prod(
+                    (my_phi ** om) * (Bk ** (1 - j))
                 )
-                laM *= m ** eta
+    if np.all(BB == 0):
+        return np.inf
+    return -np.log(LA.solve(BB, np.ones(r))[-1])
 
-                BB = np.zeros((r, r))  # To be the Ball matrix
-                for jd in range(0, r):
-                    j = decimal_to_bit_array(jd, m)
-                    for omd in range(0, jd + 1):
-                        om = decimal_to_bit_array(omd, m)
-                        if np.all(om <= j):
-                            ## divide by zero encountered in double_scalars
-                            ## overflow encountered in double_scalars
-                            my_phi = phi((1-j) @ laM, logtheta)
+@njit(parallel=True, fastmath=True, cache=True)
+def mynll(x, Y, XX, fsize):
+    #print(x)
+    llaL = x[0]
+    llaG = x[1]
+    logtheta = x[2]
+    eta = (4.0 / np.pi) * np.arctan(x[3])
+    alpha = x[4 : (4 + fsize)]
+    beta = x[(4 + fsize) : (4 + 2 * fsize)]
+    gamma = x[(4 + 2 * fsize) :]
 
-                            if np.any(
-                                np.floor(np.log10(np.abs(my_phi[my_phi != 0]))) < -100
-                            ):
-                                return np.inf
+    num_households = len(Y)
+    nlv = np.zeros(num_households)  # Vector of negative log likelihoods
 
-                            BB[jd, omd] = 1.0 / np.prod(
-                                (my_phi ** om) * (Bk ** (1 - j))
-                            )
-                nlv[i] = -np.log(LA.solve(BB, np.ones(r))[-1])
-        nll = np.sum(nlv)
-        # nll += 7.4*np.sum(x**2) # Add a Ridge if needed
-        nll += np.sum(x ** 2)  # Add a Ridge if needed
-        return nll
-    else:
-        # This was a try/except block but these are not supported by numba. TODO: work out and implement correct branching logic
-        nll = np.inf
-        return nll
+    for i in prange(num_households):
+        nlv[i] = nll(XX[i], Y[i], llaL, llaG, logtheta, eta, alpha, beta, gamma)
+
+    # return the nll sum, with a Ridge 
+    return np.sum(nlv) + np.sum(x ** 2) 
+
+def optimise_gradient_descent(x0, bb, Y, XX, feature, nrestarts):
+    def callbackF(x, x2=0.0, x3=0.0):
+        log.info(
+            "Evaluated at [{:.3f},{:.3f},{:.3f},{:.3f},{:.3f},{:.3f},{:.3f}]: {:.8f}".format(
+                x[0], x[1], x[2], x[3], x[4], x[5], x[6], mynll(x, Y, XX, feature.size)
+            )
+        )
+
+    samples = []
+
+    nll0 = mynll(x0, Y, XX, feature.size)
+    log.info(f"Descending from: {x0} distance {nll0}")
+    try:
+        minimum = op.minimize(
+            mynll,
+            x0,
+            (Y, XX, feature.size),
+            method="TNC",
+            callback=callbackF,
+            bounds=bb,
+            options={"maxiter": 10000},
+        )
+        log.info(f"Found minimum: {minimum.x}")
+        samples.append(minimum)
+    except Exception as e:
+        log.error(f"Failure starting from {x0} restarting:")
+        log.error(f"{e}")
+
+    ## Find different minima from a selection of starting points.
+    while len(samples) < nrestarts + 1:
+        nll0 = np.nan
+        try:
+            while (np.isnan(nll0)) or (np.isinf(nll0)):
+                xx0 = np.random.uniform(bb[:, 0], bb[:, 1])
+                nll0 = mynll(xx0, Y, XX, feature.size)
+            log.info(f"Descending from: {xx0} - {nll0}")
+            minimum = op.minimize(
+                mynll,
+                xx0,
+                (Y, XX, feature.size),
+                bounds=bb,
+                method="TNC",
+                callback=callbackF,
+                options={"maxiter": 1000, "ftol": 1e-9},
+            )
+            log.info(f"Found minimum: {minimum.x}")
+            samples.append(minimum)
+        except Exception as e:
+            log.error(f"Failure starting from {xx0} restarting:")
+            log.error(f"{e}")
+
+    ff = np.inf * np.ones(len(samples))
+    for i in range(len(samples)):  # In case of crash - XXX ww???
+        if samples[i].success:
+            ff[i] = samples[i].fun
+
+    xhat = samples[ff.argmin()].x
+
+    return xhat
 
 def command():
     parser = argparse.ArgumentParser()
     parser.add_argument("--nrestarts", type=int, default=1, help="Number of times to run the fitting process")
-    parser.add_argument("--seed", type=int, default=46, help="Random seed")
-    parser.add_argument("--lower", type=int, default=0, help="Lower age cutoff")
-    parser.add_argument("--upper", type=int, default=20, help="Upper age cutoff")
+    parser.add_argument("--lower", type=int, default=13, help="Lower age cutoff")
+    parser.add_argument("--upper", type=int, default=30, help="Upper age cutoff")
     parser.add_argument("graph", help="GraphML file containing network")
 
     args = parser.parse_args()
 
-    Y, XX = read_graph_data(args.graph, cutoff=(args.lower, args.upper))
+    feature=feature_age_band
+    fargs={"band": (args.lower, args.upper) }
+    Y, XX = read_graph_data(args.graph, feature, **fargs)
     num_households = len(Y)
-    na = 1
 
     # Indicative parameters - to do, add bounds and mulitple restarts
-    x0 = np.array(
-        [
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-        ]
-    )
-    mynll(x0, Y, XX)
+    x0 = np.zeros(7)
 
     ## bounds for the optimization. the x is constrained to remain
     ## within these ranges.
@@ -198,59 +260,7 @@ def command():
         ]
     )
 
-    def callbackF(x, x2=0.0, x3=0.0):
-        log.info(
-            "Evaluated at [{:.3f},{:.3f},{:.3f},{:.3f},{:.3f},{:.3f},{:.3f}]: {:.8f}".format(
-                x[0], x[1], x[2], x[3], x[4], x[5], x[6], mynll(x, Y, XX)
-            )
-        )
-
-    ## XXX the below should be restructured. why do we set the seed only after the
-    ## first iteration? why do we have different options to the minimizer?
-    foutstore = []
-    fout = op.minimize(
-        mynll,
-        x0,
-        (Y, XX),
-        method="TNC",
-        callback=callbackF,
-        bounds=bb,
-        options={"maxiter": 10000},
-    )
-    foutstore.append(fout)
-
-    np.random.seed(args.seed)
-
-    ## Find different minima from a selection of starting points.
-    log.info("nrestarts: {}".format(args.nrestarts))
-    for k in range(0, args.nrestarts):
-        nll0 = np.nan
-        while (np.isnan(nll0)) or (np.isinf(nll0)):
-            xx0 = np.random.uniform(bb[:, 0], bb[:, 1])
-            nll0 = mynll(xx0, Y, XX)
-        try:
-            log.info("Starting at: {} - {}".format(xx0, nll0))
-            fout = op.minimize(
-                mynll,
-                xx0,
-                (Y, XX),
-                bounds=bb,
-                method="TNC",
-                callback=callbackF,
-                options={"maxiter": 1000, "ftol": 1e-9},
-            )
-            log.info("Found: {}".format(fout.x))
-            foutstore.append(fout)
-        except:
-            k -= 1
-
-    ff = np.inf * np.ones(len(foutstore))
-    for i in range(0, len(foutstore)):  # In case of crash
-        if foutstore[i].success:
-            ff[i] = foutstore[i].fun
-
-
-    xhat = foutstore[ff.argmin()].x
+    xhat = optimise_gradient_descent(x0, bb, Y, XX, feature, args.nrestarts)
     log.info("xhat = {}".format(xhat))
 
     pn = len(x0)
@@ -266,18 +276,18 @@ def command():
         for k in range(0, j):
             ek[k] = dx[k]
             Hinv[j, k] = (
-                mynll(xhat + ej + ek, Y, XX)
-                - mynll(xhat + ej - ek, Y, XX)
-                - mynll(xhat - ej + ek, Y, XX)
-                + mynll(xhat - ej - ek, Y, XX)
+                mynll(xhat + ej + ek, Y, XX, feature.size)
+                - mynll(xhat + ej - ek, Y, XX, feature.size)
+                - mynll(xhat - ej + ek, Y, XX, feature.size)
+                + mynll(xhat - ej - ek, Y, XX, feature.size)
             )
             ek[k] = 0.0
         Hinv[j, j] = (
-            -mynll(xhat + 2 * ej, Y, XX)
-            + 16 * mynll(xhat + ej, Y, XX)
-            - 30 * mynll(xhat, Y, XX)
-            + 16 * mynll(xhat - ej, Y, XX)
-            - mynll(xhat - 2 * ej, Y, XX)
+            -mynll(xhat + 2 * ej, Y, XX, feature.size)
+            + 16 * mynll(xhat + ej, Y, XX, feature.size)
+            - 30 * mynll(xhat, Y, XX, feature.size)
+            + 16 * mynll(xhat - ej, Y, XX, feature.size)
+            - mynll(xhat - 2 * ej, Y, XX, feature.size)
         )
         ej[j] = 0.0
     Hinv += np.triu(Hinv.T, 1)
